466fcbdffea6345a27013a0439dd0373077dad90
GMorrow11 <gavin.d.morrow@icloud.com>
2025-09-05 19:49:27 -0500

    added scripts and stuff fr fr no cap

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
+#script runs on import, runs indefinetly so I dont have to make a function for the if running area
 import pygameRunner
 import pygame
-screen = pygame.display.set_mode((1200, 1200))
-running = True
-while running == True:
-    screen.fill((54, 89, 74)) # Green background
-    #center of the screen is something like (550,350)
-    screen.blit(pygameRunner.cardImg,(550,500))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.flip()
-pygame.quit()
--- pygameRunner.py
+++ pygameRunner.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-
+#try making the card change every second, watch python video for how to do it
 pygame.init()
 cards = [
     'AceH', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'H11', 'H12', 'H13',
@@ -8,15 +8,17 @@ cards = [
     'AceC', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12', 'C13',
     'AceS', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11', 'S12', 'S13'
 ]
-
-activeCard = cards[random.randint(0, len(cards) - 1)]
-
 screen = pygame.display.set_mode((1200, 1200))
-cardImg = pygame.image.load('/Users/Gavin/Documents/Python/Swoop/Cards Copy/' + str(activeCard) + '.png').convert_alpha()
-cardImg = pygame.transform.scale(cardImg, (cardImg.get_width() * 0.4, cardImg.get_height() * 0.4))
-
+delta_time = 0.1
+def random_card():
+    global activeCard
+    global cardImg
+    activeCard = cards[random.randint(0, len(cards) - 1)]
+    cardImg = pygame.image.load('/Users/Gavin/Documents/Python/Swoop/Cards Copy/' + str(activeCard) + '.png').convert_alpha()
+    cardImg = pygame.transform.scale(cardImg, (cardImg.get_width() * 0.4, cardImg.get_height() * 0.4))
+random_card()
 running = True
-while running == True:
+while running:
     screen.fill((54, 89, 74)) # Green background
     #center of the screen is something like (550,350)
     screen.blit(cardImg,(550,500))
